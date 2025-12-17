2eadd835121017cf8d00acc0a184a14c0c6ed100
Ayush kumar <ayush@Ayushs-MacBook-Air-3.local>
2025-12-17 22:29:08 +0530

    just saw Id declaration in dto and corrected

--- src/main/java/com/ums/backend/dto/DepartmentRequestDto.java
+++ src/main/java/com/ums/backend/dto/DepartmentRequestDto.java
@@ -1,12 +1,10 @@
 package com.ums.backend.dto;
 
 
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 @Data
 public class DepartmentRequestDto {
-    @Id
     @NotBlank
     private String departmentId;
     @NotBlank
--- src/main/java/com/ums/backend/dto/DepartmentResponseDto.java
+++ src/main/java/com/ums/backend/dto/DepartmentResponseDto.java
@@ -2,13 +2,11 @@ package com.ums.backend.dto;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class DepartmentResponseDto {
-    @Id
     @NotBlank
     private String departmentId;
     @NotBlank
